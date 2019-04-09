b9160bb1a8cb40b2401ebdc53da5f844c14916c1
Preston <Prestooonnn@gmail.com>
2019-04-10 00:17:32 +0200

    Going to be testing JSON serialization with Mono
    Placeholding for edubacation

---
+++ jwrite/jwrite/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+using System.Text;
+namespace jwrite
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("Text: ");
+            string text = Console.ReadLine();
+            StreamWriter writer = new StreamWriter("file.txt", false, Encoding.UTF8);
+            writer.Write(text);
+
+            writer.Close();
+
+
+        }
+    }
+}
