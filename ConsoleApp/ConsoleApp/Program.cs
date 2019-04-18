242b467fbe532fdddd3f4c4795756d8cdfbeafd4
Preston <Prestooonnn@gmail.com>
2019-04-18 22:37:16 +0200

    Just testing.
    Refreshing my memory, doing simple conversions and foreach loop.

---
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(a + b);
+            
+            if(a <= 60 && b <= 60)
+            {
+                Console.WriteLine(a + b + " Less than 60");
+            }
+            else
+            {
+               Console.WriteLine("Is not less than 60");
+            }
+
+            int[] numArray = new int[]{15, 16, 20, 25, 18, 26};
+            int pullArray = numArray.Length;
+            Console.WriteLine(pullArray);
+
+            if (pullArray >= 3)
+            {
+               Console.WriteLine("There are three or less elements in this array.");
+            }
+            else 
+            {
+                Console.WriteLine("There are more than 3 elements in this array");
+            }
+            Console.WriteLine("How many elements are in the array?");
+
+            foreach(int numbers in numArray)
+            {
+                pullArray = Convert.ToInt32(Console.ReadLine());
+                if (pullArray == numArray.Length)
+                {
+                    Console.WriteLine("Correct");
+                }
+                else
+                {
+                    Console.WriteLine("Guess again");
+                }
+            }
+            
+        }
+    }
+}
