4eadbbf04fda947b720360d490fa6bcec2fe1a15
Preston <Prestooonnn@gmail.com>
2019-04-22 22:48:41 +0200

    Balance
    Balance

---
+++ Balance/Balance/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Balance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int xs = 0;
+            int ys = 0;
+            
+            Console.WriteLine("Enter the X and the Y and see if its perfectly balanced.");
+            char[] userInput = Console.ReadLine().ToCharArray();
+
+            foreach (char c in userInput)
+            {
+                if (c == 'x')
+                {
+                    xs++;
+                }
+                else if (c == 'y') ;
+                {
+                    ys++;
+                }
+            }
+            if (xs == ys)
+            {
+                Console.WriteLine("Perfectly Balanced");
+            }
+            else
+            {
+                Console.WriteLine("Not Balanced.");
+            }
+        }
+    }
+}
